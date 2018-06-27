2e8e071e51504231ff6af1dbbf305fe2518486ab
Subzero <dima@svalov.ru>
2018-06-27 17:18:02 -0300

    uncomment

--- email-photo-booth.py
+++ email-photo-booth.py
@@ -32,11 +32,11 @@ def check_photo_request(hostname, username, password, boss):
     return command_photo
 
 
-# def take_picture(image_file, rotation):
-#    from picamera import PiCamera
-#    cam = PiCamera()
-#    cam.rotation = rotation
-#    cam.capture(image_file)
+def take_picture(image_file, rotation):
+    from picamera import PiCamera
+    cam = PiCamera()
+    cam.rotation = rotation
+    cam.capture(image_file)
 
 
 def read_image_data(file_path):
@@ -89,7 +89,7 @@ logger.debug('email-photo-booth started')
 try:
     if check_photo_request(imaphost, username, password, boss):
         logger.debug('trying to take photo')
-        # take_picture(imagefile, rotation)
+        take_picture(imagefile, rotation)
         logger.debug('photo taken to ' + imagefile)
         logger.debug('trying to read photo from ' + imagefile)
         image_data = read_image_data(imagefile)
